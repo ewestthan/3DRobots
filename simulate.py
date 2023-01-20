8aa77bda1aaa006fd4a5fdae7f2340e8f0ed5663
Ethan Patrick West <ethan.west@uvm.edu>
2023-01-19 20:18:43 -0500

    Added pyrosim

--- simulate.py
+++ simulate.py
@@ -2,9 +2,9 @@ import pybullet as p
 import time
 physicsClient = p.connect(p.GUI)
 
-for i in range(1000):
+for i in range(120):
     p.stepSimulation()
-    time.sleep(0.5)
+    time.sleep(1)
     print(i)
 
 p.disconnect()
